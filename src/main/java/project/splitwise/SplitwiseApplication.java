e1e9c949e2e44f2dae9d0bc6b87df59b3e4b7664
Rahul Bhiwre <rahulbhiwre@gmail.com>
2023-10-12 20:41:44 +0530

    added h2 database

--- src/main/java/project/splitwise/SplitwiseApplication.java
+++ src/main/java/project/splitwise/SplitwiseApplication.java
@@ -10,10 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@ComponentScan(basePackages=("project.**"))
-@EnableJpaRepositories("project.**")
-@EntityScan("project.**")
-@CrossOrigin
+//@ComponentScan(basePackages=("project.**"))
+//@EnableJpaRepositories("project.**")
+//@EntityScan("project.**")
+//@CrossOrigin
 public class SplitwiseApplication {
 
 	public static void main(String[] args) {
